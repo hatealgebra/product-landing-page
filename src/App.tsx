44e81dc8dd5f2a173f45e8a39bf16286ec6e6f6f
Pavel Vondra <pavelvondra66@gmail.com>
2025-05-30 09:23:18 +0200

    feat: Init setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+      </header>
+    </div>
+  )
+}
+
+export default App
